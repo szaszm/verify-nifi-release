0e6823dd35b6e3d0a896f1da1e385f041131cea1
Marton Szasz <szaszm@apache.org>
2026-04-21 16:08:49 +0200

    minificpp-merge-pr.sh: disable globbing in PR data

--- bin/minificpp-merge-pr.sh
+++ bin/minificpp-merge-pr.sh
@@ -20,32 +20,32 @@ cleanup_and_die() {
 
 pull_id=$1
 
-pull_json="$(curl -s -S -H "Accept: application/vnd.github+json" https://api.github.com/repos/apache/nifi-minifi-cpp/pulls/$pull_id)"
-repo_user="$(echo $pull_json | jq -r '.head.user.login')"
-author="$(echo $pull_json | jq -r '.user.login')"
-branch="$(echo $pull_json | jq -r '.head.ref')"
-title="$(echo $pull_json | jq -r '.title')"
+pull_json="$(curl -s -S -H "Accept: application/vnd.github+json" "https://api.github.com/repos/apache/nifi-minifi-cpp/pulls/${pull_id}")"
+repo_user="$(echo "${pull_json}" | jq -r '.head.user.login')"
+author="$(echo "${pull_json}" | jq -r '.user.login')"
+branch="$(echo "${pull_json}" | jq -r '.head.ref')"
+title="$(echo "${pull_json}" | jq -r '.title')"
 # modified sed expression to remove trailing blank lines from https://stackoverflow.com/questions/7359527/removing-trailing-starting-newlines-with-sed-awk-tr-and-friends
-body="$(echo $pull_json | jq -r '.body' | sed -e '/^Thank you for submitting a contribution/,$d' -e '/^\s*https:\/\/issues.apache.org\/jira/d' -e '/Depends on https:\/\/github.com/d' | sed -e :a -e '/[^[:blank:]]/,$!d; /^[[:space:]_\-]*$/{ $d; N; ba' -e '}' | sed -e 's/\r//g')"
-base_branch="$(echo $pull_json | jq -r '.base.ref')"
+body="$(echo "${pull_json}" | jq -r '.body' | sed -e '/^Thank you for submitting a contribution/,$d' -e '/^\s*https:\/\/issues.apache.org\/jira/d' -e '/Depends on https:\/\/github.com/d' | sed -e :a -e '/[^[:blank:]]/,$!d; /^[[:space:]_\-]*$/{ $d; N; ba' -e '}' | sed -e 's/\r//g')"
+base_branch="$(echo "${pull_json}" | jq -r '.base.ref')"
 
 # assuming remote == author
-remote=$repo_user
+remote="${repo_user}"
 if [ "${repo_user}" = "apache" ]; then
 	remote=upstream
 fi
 
-echo base branch: $(echo $pull_json | jq -r '.base.ref')
-echo title: $title
-echo repo_user: $repo_user
-echo author: $author
-echo remote: $remote
-echo branch: $branch
+echo base branch: "$(echo "${pull_json}" | jq -r '.base.ref')"
+echo title: "${title}"
+echo repo_user: "${repo_user}"
+echo author: "${author}"
+echo remote: "${remote}"
+echo branch: "${branch}"
 echo ----------------------------------------------------
 echo "${body}"
 echo ----------------------------------------------------
 
-message_args=(-m "$title")
+message_args=(-m "${title}")
 if [ -n "${body}" ]; then
 	message_args=("${message_args[@]}" "-m" "${body}")
 fi
@@ -54,15 +54,15 @@ message_args=("${message_args[@]}" "-m" "Closes #${pull_id}")
 set -x
 git checkout main
 echo Delete leftover merge branches if any. Failure is normal and ignored.
-git branch -D merge-${pull_id} || :
+git branch -D "merge-${pull_id}" || :
 git pull
-git fetch $remote
-git checkout $remote/$branch
-git checkout -b merge-${pull_id} --track $remote/$branch
+git fetch "${remote}"
+git checkout "${remote}/${branch}"
+git checkout -b "merge-${pull_id}" --track "${remote}/${branch}"
 git pull --rebase
-git rebase --onto main upstream/$base_branch merge-${pull_id} || (echo 'Rebase failed. Starting subshell to resolve conflicts. Run "exit 0" if you successfully finished the rebase, or "exit 1" if you want to abort'; bash) || cleanup_and_die ${pull_id}
+git rebase --onto main "upstream/${base_branch}" "merge-${pull_id}" || (echo 'Rebase failed. Starting subshell to resolve conflicts. Run "exit 0" if you successfully finished the rebase, or "exit 1" if you want to abort'; bash) || cleanup_and_die "${pull_id}"
 git checkout main
-git merge --squash --no-commit merge-${pull_id} || cleanup_and_die ${pull_id}
+git merge --squash --no-commit "merge-${pull_id}" || cleanup_and_die "${pull_id}"
 author_name_email="$(git log --format='%an <%ae>' "${base_branch}...merge-${pull_id}" | tail -1)"
-git branch -D merge-${pull_id}
+git branch -D "merge-${pull_id}"
 git commit --signoff --gpg-sign --author="$author_name_email" "${message_args[@]}"
